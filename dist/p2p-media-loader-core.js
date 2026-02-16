2805619f0847c3e3369e79b145158789dd487704
p2pmediacloud-hub <p2pmediacloud@gmail.com>
2026-02-16 10:50:03 +0500

    Update p2p-media-loader-core.js

--- dist/p2p-media-loader-core.js
+++ dist/p2p-media-loader-core.js
@@ -3266,10 +3266,10 @@
       {
         urls: [
           "stun:stun.l.google.com:19302",
-          'stun:stun1.l.google.com:19302',
-          'stun:stun2.l.google.com:19302',
-          'stun:stun3.l.google.com:19302',
-          'stun:stun4.l.google.com:19302',
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302',
+          "stun:stun4.l.google.com:19302",
           "stun:global.stun.twilio.com:3478"
         ]
       }
@@ -6811,6 +6811,10 @@ peerId: ${peerId}`
       rtcConfig: {
         iceServers: [
           { urls: "stun:stun.l.google.com:19302" },
+          { urls: "stun:stun1.l.google.com:19302" },
+          { urls: "stun:stun2.l.google.com:19302" },
+          { urls: "stun:stun3.l.google.com:19302" },
+          { urls: "stun:stun4.l.google.com:19302" },
           { urls: "stun:global.stun.twilio.com:3478" }
         ]
       },
